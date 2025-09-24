a3bf8b823992909746bc46ed96e3257af4f7a5bb
Hyoung-Seok <dkavmdk98@gmail.com>
2025-09-24 23:58:43 +0900

    [Rec] 탈출 조건이 없는 재귀 함수 작성

--- (C#)Project/MainProc.cs
+++ (C#)Project/MainProc.cs
@@ -4,6 +4,7 @@ public static class MainProc
 {
     public static void Main(string[] args)
     {
-       
+        var test = new RecursiveTest();
+        test.Recursive();
     }
 }
---
+++ (C#)Project/Recursive.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class RecursiveTest
+{
+    public void Recursive()
+    {
+        Console.WriteLine("Recursive Call!");
+        // 자기 자신 호출
+        Recursive();
+    }
+}
