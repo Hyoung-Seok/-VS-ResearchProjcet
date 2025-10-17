cd3eafab3ae44af6236537f0c9978480acfabdff
Hyoung-Seok <dkavmdk98@gmail.com>
2025-10-17 18:23:52 +0900

    [List] 배열 기반 리스트 헤더 파일 작성

---
+++ (C++)Project/ArrayList.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+typedef int LData;
+constexpr int LEN = 100;
+
+struct ArrayList
+{
+	LData arr[LEN];
+	int count;
+	int cur_position;
+};
+
+// 추상 자료형(ADT)
+void InitList(ArrayList* pList);
+void Insert(ArrayList* pList, LData data);
+
+void First(ArrayList* pList, LData* data);
+void Next(ArrayList* pList, LData* data);
+
+LData Remove(ArrayList* pList);
+int Count(ArrayList* pList);
