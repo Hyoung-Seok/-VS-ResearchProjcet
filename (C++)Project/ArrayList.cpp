a13ed54cd4eab7c22c58aeddbdfcdaf1200e379d
Hyoung-Seok <dkavmdk98@gmail.com>
2025-10-17 19:27:46 +0900

    [List] 배열 기반 리스트 구현부 작성

---
+++ (C++)Project/ArrayList.cpp
@@ -0,0 +1,74 @@
+﻿#include "ArrayList.h"
+#include <iostream>
+using namespace std;
+
+void InitList(ArrayList* pList)
+{
+	pList->count = 0;
+	pList->curPos = 0;
+}
+
+void Insert(ArrayList* pList, LData data)
+{
+	if(pList->count >= LEN)
+	{
+		cout << "더 이상 저장할 수 없습니다." << endl;
+		return;
+	}
+
+	pList->arr[pList->count++] = data;
+}
+
+bool First(ArrayList* pList, LData* data)
+{
+	if (pList->count <= 0)
+		return false;
+
+	pList->curPos = 0;
+	*data = pList->arr[0];
+	return true;
+}
+
+bool Next(ArrayList* pList, LData* data)
+{
+	if (pList->curPos >= pList->count - 1)
+		return false;
+
+	*data = pList->arr[++pList->curPos];
+	return true;
+}
+
+LData Remove(ArrayList* pList)
+{
+	if (pList->count <= 0)
+	{
+		cout << "삭제할 데이터가 없습니다." << endl;
+		return LData();
+	}
+
+	const LData result = pList->arr[pList->curPos];
+
+	for(int i = pList->curPos; i < pList->count - 1; ++i)
+	{
+		pList->arr[i] = pList->arr[i + 1];
+	}
+
+	pList->count--;
+	pList->curPos--;
+
+	return result;
+}
+
+int Count(ArrayList* pList)
+{
+	return pList->count;
+}
+
+void PrintAllValue(const ArrayList* const pList)
+{
+	for(int i = 0; i < pList->count; ++i)
+	{
+		cout << pList->arr[i] << ", ";
+	}
+	cout << endl;
+}
--- (C++)Project/MainProc.cpp
+++ (C++)Project/MainProc.cpp
@@ -1,7 +1,39 @@
-#include<iostream>
+﻿#include<iostream>
+#include "ArrayList.h"
 using namespace std;
 
 int main()
 {
+	ArrayList array_list;
+
+	InitList(&array_list);
+
+	// 배열 기반 리스트에 값 삽입
+	for(int i = 0; i < 100; ++i)
+	{
+		Insert(&array_list, i);
+	}
+
+	// 값 확인
+	LData data;
+	First(&array_list, &data);
+	cout << data << ", ";
+
+	while(Next(&array_list, &data) == true)
+	{
+		cout << data << ", ";
+	}
+	cout << endl;
+
+	// 첫 번째 값 제거
+	First(&array_list, &data);
+	data = Remove(&array_list);
+
+	// 값 제거 후 확인
+	cout << "제거된 데이터 : " << data << endl;
+	cout << "리스트 수 : " << Count(&array_list) << endl;
+
+	PrintAllValue(&array_list);
+
 	return 0;
 }
