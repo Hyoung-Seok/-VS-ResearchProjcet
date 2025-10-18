da6801d22e46151b652c77e8f3ae502764e66ce2
Hyoung-Seok <dkavmdk98@gmail.com>
2025-10-18 21:12:35 +0900

    [Link] 단일 리스트의 헤더 파일 구현

---
+++ (C++)Project/LinkedList.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+typedef int LData;
+
+struct Node
+{
+	LData data;
+	Node* nextNode = nullptr;
+};
+
+struct LinkedList
+{
+	Node* head = nullptr;
+	Node* cur = nullptr;
+	Node* prev = nullptr;
+	int count = 0;
+};
+
+typedef LinkedList List;
+
+void InitList(List* p_list);
+void Add(List* p_list, LData data);
+
+bool First(List* p_list, LData* data);
+bool Next(List* p_list, LData* data);
+
+LData Remove(List* p_list);
+
+int Count(List* p_list);
