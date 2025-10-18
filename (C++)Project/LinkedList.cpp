b72dd696e9a922fcf337ac630bc9e246a287583c
Hyoung-Seok <dkavmdk98@gmail.com>
2025-10-18 21:12:52 +0900

    [Link] 단일 리스트의 구현 파일 구현

---
+++ (C++)Project/LinkedList.cpp
@@ -0,0 +1,97 @@
+﻿#include "LinkedList.h"
+#include "iostream"
+using namespace std;
+
+void InitList(List* p_list)
+{
+	if (p_list == nullptr) return;
+
+	// head가 존재하지 않는다면
+	if(p_list->head == nullptr)
+	{
+		p_list->head = new Node;
+		return;
+	}
+
+	// head에 다음 데이터가 존재한다면
+	if(p_list->head->nextNode != nullptr)
+	{
+		auto cur = p_list->head->nextNode;
+		while(cur != nullptr)
+		{
+			auto next = cur->nextNode;
+			delete cur;
+			cur = next;
+		}
+
+		p_list->head->nextNode = nullptr;
+		p_list->cur = nullptr;
+		p_list->count = 0;
+	}
+}
+
+void Add(List* p_list, LData data)
+{
+	auto node = new Node;
+	node->data = data;
+
+	auto firstNode = p_list->head->nextNode;
+	node->nextNode = firstNode;
+	p_list->head->nextNode = node;
+
+	p_list->count++;
+}
+
+bool First(List* p_list, LData* data)
+{
+	if(p_list == nullptr || p_list->head == nullptr)
+	{
+		cout << "저장된 데이터가 없습니다!" << endl;
+		return false;
+	}
+
+	p_list->cur = p_list->head->nextNode;
+	p_list->prev = p_list->head;
+
+	*data = p_list->cur->data;
+
+	return true;
+}
+
+bool Next(List* p_list, LData* data)
+{
+	if(p_list->cur->nextNode == nullptr)
+	{
+		return false;
+	}
+
+	p_list->prev = p_list->cur;
+	p_list->cur = p_list->cur->nextNode;
+
+	*data = p_list->cur->data;
+	return true;
+}
+
+LData Remove(List* p_list)
+{
+	if(p_list->count == 0)
+	{
+		cout << "삭제할 데이터가 없습니다." << endl;
+	}
+
+	auto deleteNode = p_list->cur;
+	auto value = deleteNode->data;
+
+	p_list->prev->nextNode = deleteNode->nextNode;
+	p_list->cur = p_list->prev;
+
+	delete deleteNode;
+	p_list->count--;
+
+	return value;
+}
+
+int Count(List* p_list)
+{
+	return p_list->count;
+}
--- (C++)Project/MainProc.cpp
+++ (C++)Project/MainProc.cpp
@@ -1,7 +1,55 @@
-#include<iostream>
+﻿#include<iostream>
+#include "LinkedList.h"
 using namespace std;
 
 int main()
 {
+	LinkedList list;
+
+	InitList(&list);
+
+	// 10개의 데이터 삽입
+	for(int i = 0; i < 10; ++i)
+	{
+		Add(&list, i + 1);
+	}
+
+	// 데이터 확인
+	auto data = 0;
+	int index = 1;
+
+	if(First(&list, &data) == true)
+	{
+		cout << index << "번 데이터 : " << data << endl;
+		index++;
+
+		while(Next(&list, &data) == true)
+		{
+			cout << index << "번 데이터 : " << data << endl;
+			index++;
+		}
+
+		cout << endl;
+	}
+
+	//첫 번째 데이터 삭제
+	First(&list, &data);
+	Remove(&list);
+	index = 1;
+
+	if (First(&list, &data) == true)
+	{
+		cout << index << "번 데이터 : " << data << endl;
+		index++;
+
+		while (Next(&list, &data) == true)
+		{
+			cout << index << "번 데이터 : " << data << endl;
+			index++;
+		}
+
+		cout << endl;
+	}
+
 	return 0;
 }
