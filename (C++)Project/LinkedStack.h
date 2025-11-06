0569cc1f3297626b484dd9a393c29b02351d2d81
Hyoung-Seok <dkavmdk98@gmail.com>
2025-11-07 01:45:06 +0900

    [Stack] 연결 리스트 기반 스택 구현

---
+++ (C++)Project/LinkedStack.h
@@ -0,0 +1,23 @@
+#pragma once
+typedef int Data;
+
+struct Node
+{
+	Data data = 0;
+	Node* next = nullptr;
+};
+
+struct LinkedStack
+{
+	Node* head = nullptr;
+};
+
+typedef LinkedStack Stack;
+
+void LInit(Stack* p_stack);				// 스택 초기화
+
+void LPush(Stack* p_stack, Data data);	// 스택에 데이터 넣기
+Data LPop(Stack* p_stack);				// 스택에서 데이터 꺼내기
+Data LPeek(Stack* p_stack);				// 최상위 데이터 확인
+
+bool LIsEmpty(Stack* p_stack);			// 스택이 비었는지 확인
