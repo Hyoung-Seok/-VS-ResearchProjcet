6f427ca9513a8791a8d9c87572ae270913237f5b
Hyoung-Seok <dkavmdk98@gmail.com>
2025-11-06 17:49:15 +0900

    [Stack] 배열 기반 스택 구현부 작성

---
+++ (C++)Project/ArrayStack.cpp
@@ -0,0 +1,55 @@
+﻿#include "ArrayStack.h"
+#include<iostream>
+using namespace std;
+
+void Init(Stack* p_stack)
+{
+	if(p_stack == nullptr)
+	{
+		cout << "스택이 존재하지 않습니다!" << endl;
+		return;
+	}
+
+	p_stack->topIndex = -1;
+}
+
+void Push(Stack* p_stack, Data data)
+{
+	if(p_stack == nullptr || p_stack->topIndex + 1 >= STACK_LEN)
+	{
+		cout << "스택이 존재하지 않거나 더 이상 저장할 수 없습니다!" << endl;
+		return;
+	}
+
+	p_stack->topIndex++;
+	p_stack->arr[p_stack->topIndex] = data;
+}
+
+Data Pop(Stack* p_stack)
+{
+	if(p_stack == nullptr || IsEmpty(p_stack))
+	{
+		cout << "스택이 존재하지 않거나 꺼낼 데이터가 없습니다!" << endl;
+		return -1;
+	}
+
+	Data result = p_stack->arr[p_stack->topIndex--];
+
+	return result;
+}
+
+Data Peek(Stack* p_stack)
+{
+	if(p_stack == nullptr || IsEmpty(p_stack))
+	{
+		cout << "스택이 존재하지 않거나 꺼낼 데이터가 없습니다!" << endl;
+		return -1;
+	}
+
+	return p_stack->arr[p_stack->topIndex];
+}
+
+bool IsEmpty(Stack* p_stack)
+{
+	return p_stack->topIndex == -1;
+}
--- (C++)Project/MainProc.cpp
+++ (C++)Project/MainProc.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+﻿#include<iostream>
+#include"ArrayStack.h"
 using namespace std;
 
 int main()
 {
+	Stack stack;
+	Data data = 0;
+
+	Init(&stack);
+
+	for(int i = 1; i <= 99; i += 2)
+	{
+		Push(&stack, i);
+	}
+
+	data = Peek(&stack);
+	cout << "최상위 데이터: " << data << endl;
+
+	while(IsEmpty(&stack) == false)
+	{
+		data = Pop(&stack);
+		cout << data << " ";
+	}
+
+	cout << endl;
 	return 0;
 }
