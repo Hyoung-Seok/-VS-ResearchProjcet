35b4f313a52c18725258566ad31469417ee2485e
Hyoung-Seok <dkavmdk98@gmail.com>
2025-11-07 18:46:01 +0900

    [Stack] 연결 리스트 기반 스택을 템플릿으로 교체

--- (C++)Project/LinkedStack.h
+++ (C++)Project/LinkedStack.h
@@ -1,23 +1,87 @@
 ﻿#pragma once
-typedef char Data;
+#include <iostream>
 
+template <typename T>
 struct Node
 {
-	Data data = 0;
-	Node* next = nullptr;
+	T data = 0;
+	Node<T>* next = nullptr;
 };
 
-struct LinkedStack
+template <typename T>
+class LinkedStack
 {
-	Node* head = nullptr;
+public:
+	LinkedStack()
+	{
+		head = new Node<T>();
+	}
+	~LinkedStack()
+	{
+		if (head->next != nullptr)
+		{
+			auto cur = head->next;
+			while (cur != nullptr)
+			{
+				auto next = cur->next;
+				delete cur;
+				cur = next;
+			}
+		}
+
+		delete head;
+	}
+
+	void Push(T data)
+	{
+		if (head == nullptr)
+		{
+			return;
+		}
+
+		auto newNode = new Node<T>();
+		newNode->data = data;
+
+		// 기존 노드 연결
+		newNode->next = head->next;
+
+		// 헤드 노드와 새 노드 연결
+		head->next = newNode;
+	}
+
+	T Pop()
+	{
+		if (head == nullptr || IsEmpty() == true)
+		{
+			std::cout << "스택이 존재하지 않거나 꺼낼 데이터가 없습니다!" << std::endl;
+			return -1;
+		}
+
+		auto result = head->next->data;
+		auto delNode = head->next;
+
+		head->next = delNode->next;
+		delete delNode;
+
+		return result;
+	}
+
+	T Peek()
+	{
+		if (head == nullptr || IsEmpty())
+		{
+			std::cout << "스택이 존재하지 않거나 꺼낼 데이터가 없습니다!" << std::endl;
+			return -1;
+		}
+
+		return head->next->data;
+	}
+
+	bool IsEmpty()
+	{
+		return head->next == nullptr;
+	}
+
+private:
+	Node<T>* head = nullptr;
 };
-
-typedef LinkedStack Stack;
-
-void LInit(Stack* p_stack);				// 스택 초기화
-
-void LPush(Stack* p_stack, Data data);	// 스택에 데이터 넣기
-Data LPop(Stack* p_stack);				// 스택에서 데이터 꺼내기
-Data LPeek(Stack* p_stack);				// 최상위 데이터 확인
-
-bool LIsEmpty(Stack* p_stack);			// 스택이 비었는지 확인
