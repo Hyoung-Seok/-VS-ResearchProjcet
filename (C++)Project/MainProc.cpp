b33d91fd4b0d6f86f1f290572a28a1965dc7a6f5
Hyoung-Seok <dkavmdk98@gmail.com>
2025-12-15 18:48:06 +0900

    [Queue] 배열 기반 큐 테스트

--- (C++)Project/MainProc.cpp
+++ (C++)Project/MainProc.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include "CircularQueue.h"
 using namespace std;
 
 int main()
 {
+	CQueue queue;
+	InitQueue(&queue);
+
+	for(int i = 2; i <= 100; i += 2)
+	{
+		Enqueue(&queue, i);
+	}
+
+	while(!IsEmpty(&queue))
+	{
+		cout << Dequeue(&queue) << " ";
+	}
 	return 0;
 }
