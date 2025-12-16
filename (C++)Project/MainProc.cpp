0dfaad4b1d110a01b041bc4e0de3d079ba3d9ecb
Hyoung-Seok <dkavmdk98@gmail.com>
2025-12-17 02:00:53 +0900

    [Deque] 덱 테스트

--- (C++)Project/MainProc.cpp
+++ (C++)Project/MainProc.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
+#include "Deque.h"
 using namespace std;
 
 int main()
 {
+	Deque dq;
+	InitDeque(&dq);
+
+	for(int i = 0; i <= 50; i++)
+	{
+		if(i % 2 == 0)
+		{
+			push_front(&dq, i);
+		}
+		else
+		{
+			push_back(&dq, i);
+		}
+	}
+
+	PrintAllDataFromFront(&dq);
+
+	for(int i = 0; i <= 25; i++)
+	{
+		cout << "Pop Front: " << pop_front(&dq) << endl;
+		cout << "Pop Back: " << pop_back(&dq) << endl << endl;
+	}
+
 	return 0;
 }
